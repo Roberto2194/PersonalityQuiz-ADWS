499a8c65958436c630d4f5f82fa93718e55260bc
Roberto Liccardo <roberto_liccardo@outlook.it>
2020-05-10 12:32:37 +0200

    Initial Commit

---
+++ PersonalityQuiz-ADWS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz-ADWS
+//
+//  Created by Roberto Liccardo on 10/05/2020.
+//  Copyright © 2020 Roberto Liccardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
